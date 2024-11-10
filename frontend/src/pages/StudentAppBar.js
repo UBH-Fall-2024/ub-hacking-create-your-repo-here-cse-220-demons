0aee6522fb8680bb20e90b3cc15f9f97ecb3a0bc
mafruhamanal <mafruhamanal1@gmail.com>
2024-11-10 03:13:00 -0500

    appbar mui snippets

---
+++ frontend/src/pages/StudentAppBar.js
@@ -0,0 +1,30 @@
+import * as React from "react";
+import {
+  Box,
+  AppBar,
+  Typography,
+  Button,
+  Toolbar,
+  IconButton,
+} from "@mui/material";
+
+export default function StudentAppBar() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography
+            align="center"
+            variant="h6"
+            fontWeight="bold"
+            component="div"
+            sx={{ flexGrow: 1 }}
+          >
+            Student Assistant Portal
+          </Typography>
+          {/* <Button color="inherit">Login</Button> */}
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
