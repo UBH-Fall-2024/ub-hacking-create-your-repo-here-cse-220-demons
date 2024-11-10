4230fee9fb503db71119282276565806d7f4b1b6
aqian <aqian@buffalo.edu>
2024-11-10 06:36:08 -0500

    test

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,16 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
+//import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Student from "./pages/Student";
 import TA from "./pages/TA";
+import Register from "./pages/Register";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/student" element={<Student />} />
         <Route path="/ta" element={<TA />} />
