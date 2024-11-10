cbd2de571a06a5c5fafae1eec682fb7965baff21
aqian <aqian@buffalo.edu>
2024-11-09 22:52:43 -0500

    test

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,46 +1,35 @@
 import { useState } from "react";
-import { auth } from "./FirebaseConfig"
+import { auth } from '../Firebase.js'
 import { createUserWithEmailAndPassword } from "firebase/auth";
 
 const Login = () => {
-//I've rewritten this garbage 8 times already why is this so hard 
-//?????????
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [errorOne, errorTwo] = useState("");
-  const {load, loading} = useState(false);
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
   const handleSubmit = async (e) => {
-    await Login(email,password);
+    //await Login(email,password);
     e.preventDefault();
-    loading(true);
-    errorOne("");
+    setLoading(true);
+    setError("");
     try {
         const loginInformation = await createUserWithEmailAndPassword(auth,email,password);
-        console.log("You can connect");
+        const user = loginInformation.user;
+        setEmail("");
+        setPassword("");
     }
-    catch {
-      errorTwo("");
+    catch(errored){
+      let errorMessage = errored.message;
+      setError(errorMessage);
     }
     finally{
-        loading(false);
+        setLoading(false);
     }
     };
-    //const email = e.target.email.value;
-    //const password = e.target.password.value;
-    //createUserWithEmailAndPassword(database,email,password).then(data=>{
-        //console.log.add(data, "authdata")
-//})
-}
-
-    //try{
-      //await login(email, password);
-    //}
-  //};
-
   return (
     <form className="login" onSubmit={handleSubmit}>
       <h3>Log In</h3>
-
+     {error && <div className="error">{error}</div>}
       <label>Email address:</label>
       <input
         type="email"
@@ -52,10 +41,12 @@ const Login = () => {
         type="password"
         onChange={(e) => setPassword(e.target.value)}
         value={password}
-      />
-
-      <button >Log in</button>
-
+        disabled={loading}
+        />
+      <button type="submit" disabled={loading}>
+        {loading ? "Signing up...": "Sign up"}
+      </button>
     </form>
   );
+};
 export default Login;
